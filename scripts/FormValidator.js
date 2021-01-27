75897b954c3617d8e44786eeaa961a4b59f7d44e
Ilya <airanvalor@mail.ru>
2021-01-27 11:21:43 +0300

    Исправления после отклонения

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -1,65 +1,65 @@
 export class FormValidator {
   constructor (config, form) {
-    this.config = config;
-    this.form = form;
-    this.submitButton = this.form.querySelector('.popup__submit');
+    this._config = config;
+    this._form = form;
+    this._submitButton = this._form.querySelector('.popup__submit');
   }
 
   clearErrorMessages() {
-    this.form.querySelectorAll(this.config.errorClass).forEach(error => {
+    this._form.querySelectorAll(this._config.errorClass).forEach(error => {
       error.textContent = "";
     });
-    this.form.querySelectorAll(this.config.inputSelector).forEach(input => {
-      input.classList.remove(this.config.inputErrorClass);
+    this._form.querySelectorAll(this._config.inputSelector).forEach(input => {
+      input.classList.remove(this._config.inputErrorClass);
     });
   };
 
-  showInputError(input) {
-    const error = this.form.querySelector(`.${input.id}-error`);
+  _showInputError(input) {
+    const error = this._form.querySelector(`.${input.id}-error`);
     error.textContent = input.validationMessage;
-    input.classList.add(this.config.inputErrorClass);
+    input.classList.add(this._config.inputErrorClass);
   };
 
-  hideInputError(input) {
-    const error = this.form.querySelector(`.${input.id}-error`);
+  _hideInputError(input) {
+    const error = this._form.querySelector(`.${input.id}-error`);
     error.textContent = "";
-    input.classList.remove(this.config.inputErrorClass);
+    input.classList.remove(this._config.inputErrorClass);
   };
 
-  checkInputValidity(input) {
+  _checkInputValidity(input) {
     if (input.validity.valid) {
-      this.hideInputError(input);
+      this._hideInputError(input);
     } else {
-      this.showInputError(input);
+      this._showInputError(input);
     }
   };
 
   setButtonState(state) {
     if (state) {
-      this.submitButton.classList.remove(this.config.inactiveButtonClass);
-      this.submitButton.disabled = false;
+      this._submitButton.classList.remove(this._config.inactiveButtonClass);
+      this._submitButton.disabled = false;
     } else {
-      this.submitButton.classList.add(this.config.inactiveButtonClass);
-      this.submitButton.disabled = true;
+      this._submitButton.classList.add(this._config.inactiveButtonClass);
+      this._submitButton.disabled = true;
     };
   };
 
-  setEventListeners() {
-    const inputList = this.form.querySelectorAll(this.config.inputSelector);
+  _setEventListeners() {
+    const inputList = this._form.querySelectorAll(this._config.inputSelector);
     inputList.forEach(input => {
       input.addEventListener("input", () => {
-      this.checkInputValidity(input);
-      this.setButtonState(this.form.checkValidity());
+      this._checkInputValidity(input);
+      this.setButtonState(this._form.checkValidity());
       });
     });
   };
 
   enableFormValidation() {
-    this.setEventListeners();
-    this.form.addEventListener('submit', (evt) => {
+    this._setEventListeners();
+    this._form.addEventListener('submit', (evt) => {
       evt.preventDefault();
     });
-    this.setButtonState(this.form.checkValidity());
+    this.setButtonState(this._form.checkValidity());
   };
 }
 
